7d05cffb97f682c91af6a698f2841d8747a50481
Alex Chubaty <alex.chubaty@gmail.com>
2018-02-01 11:21:18 -0700

    add .lintr & delint

--- tests/testthat/test-numerical-comparisons.R
+++ tests/testthat/test-numerical-comparisons.R
@@ -7,27 +7,27 @@ test_that("relational operators within tolerance", {
   set.seed(123L)
   a <- jitter(1:10, 1e-3)
   b <- jitter(1:10, 1e-3)
-  less.eq    <- a %<=% b
-  less       <- a %<<% b
-  greater.eq <- a %>=% b
-  greater    <- a %>>% b
-  equal      <- a %==% b
-  notequal   <- a %!=% b
+  lessEq    <- a %<=% b
+  less      <- a %<<% b
+  greaterEq <- a %>=% b
+  greater   <- a %>>% b
+  equal     <- a %==% b
+  notEqual  <- a %!=% b
   expect_equal(less,     !greater)
-  expect_equal(notequal, !equal)
+  expect_equal(notEqual, !equal)
 
   a <- jitter(1:10, 1e-7)
   b <- jitter(1:10, 1e-7)
-  less.eq    <- a %<=% b
+  lessEq     <- a %<=% b
   less       <- a %<<% b
-  greater.eq <- a %>=% b
+  greaterEq  <- a %>=% b
   greater    <- a %>>% b
   equal      <- a %==% b
-  notequal   <- a %!=% b
+  notEqual   <- a %!=% b
   ids <- c(5L, 8L)
-  expect_equal(less[ids],        !greater[ids])
-  expect_equal(less[-ids],       greater[-ids])
-  expect_equal(less.eq,          !greater)
+  expect_equal(less[ids],  !greater[ids])
+  expect_equal(less[-ids], greater[-ids])
+  expect_equal(lessEq,     !greater)
   expect_equal(all(!equal[ids]), TRUE)
   expect_equal(all(equal[-ids]), TRUE)
 })
